7d77d5328bd258b6a80657b014c21a5d295ce7b3
Mitanshi Vyas <vyas.mit@northeastern.edu>
2025-02-06 03:53:59 -0600

    Fixed the API_KEY visibility

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# GEMINI_API_KEY = "AIzaSyBXwg9TUt61o8LnWBVQ5RNRerG_cdwkNIs"
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")  
 genai.configure(api_key=GEMINI_API_KEY)
 
