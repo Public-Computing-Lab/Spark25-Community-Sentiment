ac4973a5f747a3f3b055159f303bfaf4db12e37b
Christopher Le Dantec <c.ledantec@northeastern.edu>
2025-03-27 06:29:17 -0400

    major refactor

--- api/test/api_test.sh
+++ api/test/api_test.sh
@@ -11,29 +11,7 @@ NC='\033[0m' # No Color
 LOG_ID=0
 SESSION_ID="\"TEST_SESSION_ID\""
 
-# Function to test /data/context/token_count
-test_context_token_count(){
-	context_data='{
-		"data_selected": "",
-		"prompt_preamble":""
-	}'
-	echo -e "\n${GREEN}Testing POST /chat/token_count?context_request=experiment_5${NC}"
-	response=$(curl -s -X POST \
-		-b cookies.txt \
-		-c cookies.txt \
-		--cookie "app_version=0" \
-		-H "Content-Type: application/json" \
-		-d "$context_data" \
-		"${API_URL}/chat/token_count?context_request=experiment_5")
-	
-	if [ $? -eq 0 ]; then
-		echo "Response: $response"
-		echo -e "${GREEN}Chat endpoint test completed${NC}"
-	else
-		echo -e "${RED}Chat endpoint test failed${NC}"
-	fi
-	
-}
+
 # Function to test /data/context endpoint
 test_context_create(){
 	context_data='{
@@ -124,152 +102,51 @@ test_context_list(){
 	fi
 }
 
+# Function to test /data/context/token_count
+test_context_token_count(){	
+	echo -e "\n${GREEN}Testing GET /chat/context?context_request=experiment_5${NC}"
+	response=$(curl -X GET \
+		-b cookies.txt \
+		-c cookies.txt \
+		--cookie "app_version=0" \
+		-H "Content-Type: application/json" \
+		"${API_URL}/chat/context?context_request=experiment_5")
+	
+	if [ $? -eq 0 ]; then
+		echo "Response: $response"
+		echo -e "${GREEN}Chat endpoint test completed${NC}"
+	else
+		echo -e "${RED}Chat endpoint test failed${NC}"
+	fi
+	
+}
+
 test_context_clear(){
-	echo -e "\n${GREEN}Testing POST /chat/context/clear${NC}"
-	response=$(curl -s -X POST \
-		-b cookies.txt \
-		-c cookies.txt \
-		--cookie "app_version=0" \
-		-H "Content-Type: application/json" \
-		"${API_URL}/chat/context/clear")
-	
-	if [ $? -eq 0 ]; then
-		echo "Response: $response"
-		echo -e "${GREEN}Chat endpoint test completed${NC}"
-	else
-		echo -e "${RED}Chat endpoint test failed${NC}"
-	fi
-	
-}
-# Function to test /data endpoint
-test_data(){
-	echo -e "\n${GREEN}Testing GET /data?request=list${NC}"
-	response=$(curl -X GET \
-		-b cookies.txt \
-		-c cookies.txt \
-		--cookie "app_version=0" \
-		-H "Content-Type: application/json" \
-		"${API_URL}/data?data_request=list")
-	
-	if [ $? -eq 0 ]; then
-		echo "Response: $response"
-		echo -e "${GREEN}Chat endpoint test completed${NC}"
-	else
-		echo -e "${RED}Chat endpoint test failed${NC}"
-	fi
-	
-	echo -e "\n${GREEN}Testing GET /data?request=structured${NC}"
-	response=$(curl -X GET \
-		-b cookies.txt \
-		-c cookies.txt \
-		--cookie "app_version=0" \
-		-H "Content-Type: application/json" \
-		"${API_URL}/data?data_request=structured")
-	
-	if [ $? -eq 0 ]; then
-		echo "Response: $response"
-		echo -e "${GREEN}Chat endpoint test completed${NC}"
-	else
-		echo -e "${RED}Chat endpoint test failed${NC}"
-	fi
-	
-	echo -e "\n${GREEN}Testing GET /data?request=unstructured${NC}"
-	response=$(curl -X GET \
-		-b cookies.txt \
-		-c cookies.txt \
-		--cookie "app_version=0" \
-		-H "Content-Type: application/json" \
-		"${API_URL}/data?data_request=unstructured")
-	
-	if [ $? -eq 0 ]; then
-		echo "Response: $response"
-		echo -e "${GREEN}Chat endpoint test completed${NC}"
-	else
-		echo -e "${RED}Chat endpoint test failed${NC}"
-	fi
-	
-	echo -e "\n${GREEN}Testing GET /data?request=all${NC}"
-	response=$(curl -X GET \
-		-b cookies.txt \
-		-c cookies.txt \
-		--cookie "app_version=0" \
-		-H "Content-Type: application/json" \
-		"${API_URL}/data?data_request=all")
-	
-	if [ $? -eq 0 ]; then
-		echo "Response: $response"
-		echo -e "${GREEN}Chat endpoint test completed${NC}"
-	else
-		echo -e "${RED}Chat endpoint test failed${NC}"
-	fi
-	
-	echo -e "\n${GREEN}Testing GET /data?request=file1.csv,file2.txt${NC}"
-	response=$(curl -X GET \
-		-b cookies.txt \
-		-c cookies.txt \
-		--cookie "app_version=0" \
-		-H "Content-Type: application/json" \
-		"${API_URL}/data?data_request=Arrests_cleaned.csv,Transcript_4.txt")
-	
-	if [ $? -eq 0 ]; then
-		echo "Response: $response"
-		echo -e "${GREEN}Chat endpoint test completed${NC}"
-	else
-		echo -e "${RED}Chat endpoint test failed${NC}"
-	fi
-}
-# Function to test /chat endpoint
-#"session_id": '${SESSION_ID}',
-test_chat() {
-	echo -e "\n${GREEN}Testing POST /chat${NC}"
-	
-	chat_data='{
-		"client_query": "What are the key take aways from the data?",		
-		"app_version": "0",
+	echo -e "\n${GREEN}Testing POST /chat/context?context_request=experiment_5&option=clear${NC}"
+	context_data='{
 		"data_selected": "",
-		"data_attributes": ""
+		"prompt_preamble":""
 	}'
 	
-	echo -e "\n${GREEN}Testing POST /chat?context_request=structured${NC}"
 	response=$(curl -s -X POST \
-		-b cookies.txt \
-		-c cookies.txt \
-		--cookie "app_version=0" \
-		-H "Content-Type: application/json" \
-		-d "$chat_data" \
-		"${API_URL}/chat?context_request=structured")
-	echo "Response: $response"
-		
-	echo -e "\n${GREEN}Testing POST /chat?context_request=unstructured${NC}"
-	response=$(curl -s -X POST \
-		-b cookies.txt \
-		-c cookies.txt \
-		--cookie "app_version=0" \
-		-H "Content-Type: application/json" \
-		-d "$chat_data" \
-		"${API_URL}/chat?context_request=unstructured")
-	echo "Response: $response"
-
-	echo -e "\n${GREEN}Testing POST /chat?context_request=all${NC}"
-	response=$(curl -s -X POST \
-		-b cookies.txt \
-		-c cookies.txt \
-		--cookie "app_version=0" \
-		-H "Content-Type: application/json" \
-		-d "$chat_data" \
-		"${API_URL}/chat?context_request=all")
-	echo "Response: $response"
+	-b cookies.txt \
+	-c cookies.txt \
+	--cookie "app_version=0" \
+	-H "Content-Type: application/json" \
+	-d "$context_data" \
+	"${API_URL}/chat/context?context_request=all&option=clear")
 	
-	LOG_ID=$(echo "$response" | jq ".log_id")
-	# Check if curl command was successful
 	if [ $? -eq 0 ]; then
+		echo "Response: $response"
 		echo -e "${GREEN}Chat endpoint test completed${NC}"
 	else
 		echo -e "${RED}Chat endpoint test failed${NC}"
 	fi
+	
 }
 
-test_chat_single() {
+
+test_chat() {
 	echo -e "\n${GREEN}Testing POST /chat${NC}"
 	
 	chat_data='{
@@ -362,30 +239,61 @@ test_log_update() {
 
 # Function to test /data/query?request=
 test_data_query() {
-	echo -e "\n${GREEN}Testing GET /data/query?${NC}"
-	echo $query_string
-	# Current timestamp in ISO format
-	#timestamp=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-	# log_data='{
-	# 	"log_id": '${LOG_ID}',			
-	# 	"client_response_rating": "UPDATED"
-	# }'
 	
-	response=$(curl -X GET \
-		-b cookies.txt \
-		-c cookies.txt \
-		--cookie "app_version=0" \
-		-H "Content-Type: application/json" \
-		-d "$log_data" \
-		"${API_URL}/data/query?request=311_on_date_count&date=2023-04")
+	ENDPOINTS_OPTIONS=(
+		"311_on_date_geo&date=2019-02"
+		"311_on_date_count&date=2020-04"
+		"311_on_date_count&date=2021-06&zipcode=02121"
+		"311_year_month"
+		"311_by_type&category=living_conditions"
+		"311_by_type&category=trash"
+		"311_by_type&category=streets"
+		"311_by_type&category=parking"
+		"311_by_type&category=all"
+		"311_by_total&category=living_conditions"
+		"311_by_total&category=trash"
+		"311_by_total&category=streets"
+		"311_by_total&category=parking"
+		"311_by_total&category=all"
+		"311_by_geo&category=living_conditions"
+		"311_by_geo&category=trash"
+		"311_by_geo&category=streets"
+		"311_by_geo&category=parking"
+		"311_by_geo&category=all"
+		"911_homicides"
+		"911_shots_fired"
+		"911_shots_fired_count_confirmed"
+		"911_shots_fired_count_unconfirmed"
+		"911_homicides_and_shots_fired"
+	)
+	
+	for endpoint in "${ENDPOINTS_OPTIONS[@]}"; do
+	
+		echo -e "\n${GREEN}Testing GET /data/query?request=${endpoint}${NC}"
+		# Current timestamp in ISO format
+		#timestamp=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+		# log_data='{
+		# 	"log_id": '${LOG_ID}',			
+		# 	"client_response_rating": "UPDATED"
+		# }'
+		#read -p "Press Enter to execute this query (or type 'skip' to skip, 'quit' to exit): " user_input
 
-	# Check if curl command was successful
-	if [ $? -eq 0 ]; then
-		echo "Response: $response"
-		echo -e "${GREEN}Log endpoint test completed${NC}"
-	else
-		echo -e "${RED}Log endpoint test failed${NC}"
-	fi
+		response=$(curl -X GET \
+			-b cookies.txt \
+			-c cookies.txt \
+			--cookie "app_version=0" \
+			-H "Content-Type: application/json" \
+			-d "$log_data" \
+			"${API_URL}/data/query?request=${endpoint}")
+	
+		# Check if curl command was successful
+		if [ $? -eq 0 ]; then
+			#echo "Response Length: ${#response}"
+			echo -e "${GREEN}Log endpoint test completed${NC}"
+		else
+			echo -e "${RED}Log endpoint test failed${NC}"
+		fi
+	done
 }
 
 # Function to test /data/zipcode?request=
@@ -419,35 +327,31 @@ test_data_zip() {
 # Function to run all tests
 run_all_tests() {
 	echo -e "${GREEN}Starting API tests...${NC}"
-	test_context_create
 	test_context_list
-	test_data
+	test_context_token_count
+	test_data_query
 	test_chat
 	sleep 3
 	test_log_update
-	test_context_clear
 	echo -e "\n${GREEN}All tests completed${NC}"
 }
 
 # Main execution
 case "$1" in
-	"cache_create")
+	"context_create")
 		test_context_create
 		;;
-	"cache_list")
+	"context_list")
 		test_context_list
 		;;
-	"cache_clear")
+	"context_clear")
 		test_context_clear
 		;;
-	"data")
-		test_data
+	"context_token")
+		test_context_token_count
 		;;
 	"chat")
 		test_chat
-		;;
-	"chat_single")
-		test_chat_single
 		;;
 	"log")
 		test_log_insert
@@ -456,14 +360,11 @@ case "$1" in
 	"zip")
 		test_data_zip
 		;;
-	"db")
+	"data")
 		test_data_query
 		;;
 	"all")
 		run_all_tests
-		;;
-	"token")
-		test_context_token_count
 		;;
 	*)
 		echo "Usage: $0 [cache_create | cache_list | cache_clear | data | chat | chat_single | log | all]"
