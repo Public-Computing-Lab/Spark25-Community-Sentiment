d5ae5195ca0125d5dd37215097101bd005e73e78
Karis Moon <karis.moon2023@gmail.com>
2025-05-28 16:54:33 -0400

    Add blank map page

--- experiment-PIT/src/App.tsx
+++ experiment-PIT/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Chat from './pages/Chat'
 import Template from './pages/Template'
+import Map from './pages/Map'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Template />} />
         <Route path="/chat" element={<Chat />} />
+        <Route path="/map" element={<Map />} />
       </Routes>
       <Navbar />
     </>
