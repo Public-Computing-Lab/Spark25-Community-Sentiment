03ce22c0247978a189b30b78ef60b2f5e5f394dd
Karis Moon <karis.moon2023@gmail.com>
2025-05-28 16:21:12 -0400

    Add basic navbar

---
+++ experiment-PIT/src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <Link to="/" className="nav-link">Home</Link>
+      <Link to="/chat" className="nav-link">Chat</Link>
+    </nav>
+  )
+}
+
+export default Navbar
