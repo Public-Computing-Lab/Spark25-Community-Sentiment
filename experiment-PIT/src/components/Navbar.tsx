2dac44a3d0b15bd6c357c33834b095db75517891
Karis Moon <karis.moon2023@gmail.com>
2025-05-28 16:54:33 -0400

    Add blank map page

--- experiment-PIT/src/components/Navbar.tsx
+++ experiment-PIT/src/components/Navbar.tsx
@@ -5,6 +5,7 @@ function Navbar() {
   return (
     <nav className="navbar">
       <Link to="/" className="nav-link">Home</Link>
+      <Link to="/map" className="nav-link">Map</Link>
       <Link to="/chat" className="nav-link">Chat</Link>
     </nav>
   )
---
+++ experiment-PIT/src/pages/Map.tsx
@@ -0,0 +1,11 @@
+import './Map.css'
+
+function Map() {
+  return (
+    <div className="box">
+      <h1>Map map map</h1>
+    </div>
+  )
+}
+
+export default Map
