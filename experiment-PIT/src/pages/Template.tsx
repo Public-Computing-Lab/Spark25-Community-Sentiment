3ce5cbe952aa57da8a19cd3e2188520e29f55250
Karis Moon <karis.moon2023@gmail.com>
2025-05-28 16:27:31 -0400

    Make navbar sticky

--- experiment-PIT/src/pages/Template.tsx
+++ experiment-PIT/src/pages/Template.tsx
@@ -7,7 +7,7 @@ function Template() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className="box">
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +28,7 @@ function Template() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
