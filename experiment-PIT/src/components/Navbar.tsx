33814d8b109fc2b97374b5e49853dc01935ddba2
Karis Moon <karis.moon2023@gmail.com>
2025-05-29 12:11:09 -0400

    Make chat fill screen

--- experiment-PIT/src/components/Navbar.tsx
+++ experiment-PIT/src/components/Navbar.tsx
@@ -8,6 +8,8 @@ import HomeIcon from '@mui/icons-material/Home';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import ForumIcon from '@mui/icons-material/Forum';
 
+import { BOTTOM_NAV_HEIGHT } from '../constants/layoutConstants';
+
 const navLinks = [
   { label: 'Home', to: '/', icon: <HomeIcon /> },
   { label: 'Map', to: '/map', icon: <LocationOnIcon /> },
@@ -21,13 +23,13 @@ function Navbar() {
   const currentIndex = navLinks.findIndex(link => link.to === location.pathname);
   const [value, setValue] = React.useState(currentIndex !== -1 ? currentIndex : 0);
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+  const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
     navigate(navLinks[newValue].to);
   };
 
   return (
-    <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+    <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, height: BOTTOM_NAV_HEIGHT }} elevation={3}>
       <BottomNavigation showLabels value={value} onChange={handleChange}>
         {navLinks.map(({ label, icon }, index) => (
           <BottomNavigationAction key={index} label={label} icon={icon} />
