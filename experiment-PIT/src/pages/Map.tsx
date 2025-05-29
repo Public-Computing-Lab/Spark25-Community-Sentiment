4cff0b5824c4217508ac5ffa0586aa1e169d8e5b
Karis Moon <karis.moon2023@gmail.com>
2025-05-29 10:43:42 -0400

    Implement MUI

--- experiment-PIT/src/pages/Map.tsx
+++ experiment-PIT/src/pages/Map.tsx
@@ -1,10 +1,24 @@
-import './Map.css'
+import { Box, Typography } from '@mui/material'
 
 function Map() {
   return (
-    <div className="box">
-      <h1>Map map map</h1>
-    </div>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100%',
+        width: '100%',
+        bgcolor: 'background.paper',
+        color: 'text.primary',
+        overflow: 'hidden',
+        position: 'relative',
+        p: 2,
+      }}
+    >
+      <Typography variant="h4" component="h1" mb={2}>
+        map map
+      </Typography>
+    </Box>
   )
 }
 
