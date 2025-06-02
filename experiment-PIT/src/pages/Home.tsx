19f3479a113ef61560a753308d05b30d6f1723cb
Karis Moon <karis.moon2023@gmail.com>
2025-06-02 15:44:49 -0400

    Update home page title

--- experiment-PIT/src/pages/Home.tsx
+++ experiment-PIT/src/pages/Home.tsx
@@ -19,7 +19,7 @@ function Home() {
         Home
       </Typography>
       <Typography variant="h1" component="h1" mb={2}>
-        What's Happening in Dorchester?
+        What's is Dorchester Like?
       </Typography>
     </Box>
   )
