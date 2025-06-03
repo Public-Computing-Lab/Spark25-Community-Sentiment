4929878206ab331252843373bfb11153e8f1fd3c
Karis Moon <karis.moon2023@gmail.com>
2025-06-03 10:42:12 -0400

    Remove home page

--- experiment-PIT/src/App.tsx
+++ experiment-PIT/src/App.tsx
@@ -1,9 +1,9 @@
-import { Routes, Route } from 'react-router-dom'
-import { Box, CssBaseline } from '@mui/material'
-import Navbar from './components/Navbar'
-import Chat from './pages/Chat'
-import Template from './pages/Home'
-import Map from './pages/Map'
+import { Routes, Route } from "react-router-dom";
+import { Box, CssBaseline } from "@mui/material";
+import Navbar from "./components/Navbar";
+import Chat from "./pages/Chat";
+// import Home from "./pages/Home";
+import Map from "./pages/Map";
 
 function App() {
   return (
@@ -11,18 +11,18 @@ function App() {
       <CssBaseline />
       <Box
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          minHeight: '100vh', 
-          bgcolor: 'background.default',
-          color: 'text.primary',
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100vh",
+          bgcolor: "background.default",
+          color: "text.primary",
         }}
       >
         {/* Main content grows to fill available space */}
         <Box component="main" sx={{ flexGrow: 1 }}>
           <Routes>
-            <Route path="/" element={<Template />} />
-            <Route path="/chat" element={<Chat />} />
+            {/* <Route path="/" element={<Template />} /> */}
+            <Route path="/" element={<Chat />} />
             <Route path="/map" element={<Map />} />
           </Routes>
         </Box>
@@ -31,7 +31,7 @@ function App() {
         <Navbar />
       </Box>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
