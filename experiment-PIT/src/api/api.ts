8f0115937e8bc29a6b0903591eb7bad8945e40a2
Karis Moon <karis.moon2023@gmail.com>
2025-06-07 02:12:17 -0400

    Fix typo in payload

--- experiment-PIT/src/api/api.ts
+++ experiment-PIT/src/api/api.ts
@@ -10,7 +10,7 @@ export async function sendChatMessage(message: string, history: Message[]) {
   const url = `${import.meta.env.VITE_BASE_URL}/chat?request=experiment_pit&app_version=0.7.0&structured_response=False`
 
   const json = {
-    "client_query": message + "Here's the message history to provide context to this request: " + JSON.stringify(history),
+    "client_query": message + " Here's the message history to provide context to this request: " + JSON.stringify(history),
   };
 
   try {
