711296dca75f891b8a253167e805ff1769e5b781
Ashi Kamra <ak118@wellesley.edu>
2025-06-17 15:46:33 -0400

    changing browser router basename to specify experimenting/8

--- experiment-PIT/src/App.tsx
+++ experiment-PIT/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { CssBaseline, Box } from "@mui/material";
 import Navbar from "./components/Navbar";
 import Chat from "./pages/Chat";
@@ -7,7 +7,6 @@ import Map from "./pages/Map";
 import { MapProvider } from './components/MapProvider'
 
 function App() {
-  const location = useLocation();
 
   return (
     <MapProvider>
@@ -24,29 +23,16 @@ function App() {
         {/* Main content grows to fill available space */}
         <Box component="main" sx={{ flexGrow: 1 }}>
           <Routes>
-            {/* <Route path="/" element={<Template />} /> */}
+            {/* <Route path="/" element={<Navigate to="/experimenting/8" replace />} /> */}
             <Route path="/" element={<Chat />} />
-            <Route path="/map" element={<div />} />
+            <Route path="/map" element={<Map />} />
           </Routes>
         </Box>
-        <Box
-          sx={{
-            display: location.pathname === "/map" ? "block" : "none", // Toggle visibility based on route
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100vw",
-            height: "100vh",
-            zIndex: 0,
-          }}
-        >
-          <Map />
-        </Box>
-
         {/* Navbar sticks at bottom */}
         <Navbar />
       </Box>
     </MapProvider>
+    
   );
 }
 
